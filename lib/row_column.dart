1965ac7dc39d8a044a84107aa7a5c1a6323acb98
unknown <bidingurusmk@gmail.com>
2024-09-10 10:52:54 +0700

    rev

--- lib/row_column.dart
+++ lib/row_column.dart
@@ -7,16 +7,21 @@ class RowColum extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        color: Colors.grey[300],
         padding: EdgeInsets.all(20),
         child: Column(
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("OVO"),
+                Text(
+                  "OVO",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
                 Container(
                   padding: EdgeInsets.all(8),
                   decoration: BoxDecoration(
+                      color: Colors.white,
                       border: Border.all(),
                       borderRadius: BorderRadius.circular(20)),
                   child: Text("Promo"),
@@ -57,6 +62,9 @@ class RowColum extends StatelessWidget {
             ),
             SizedBox(height: 10),
             Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(8)),
               child: Row(
                 children: [
                   Container(
@@ -92,7 +100,119 @@ class RowColum extends StatelessWidget {
                   ),
                 ],
               ),
-            )
+            ),
+            SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(8)),
+              height: 200,
+              child: GridView.count(
+                crossAxisCount: 5,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 0,
+                padding: EdgeInsets.all(8),
+                children: [
+                  Container(
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/pulsa.png'),
+                          width: 50,
+                        ),
+                        Text(
+                          "Pulsa",
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/pln.png'),
+                          width: 50,
+                        ),
+                        Text(
+                          "PLN",
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/tv.png'),
+                          width: 50,
+                        ),
+                        Text(
+                          "Langganan TV",
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/pulsa.png'),
+                          width: 50,
+                        ),
+                        Text(
+                          "Pulsa",
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/pulsa.png'),
+                          width: 50,
+                        ),
+                        Text(
+                          "Pulsa",
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/pulsa.png'),
+                          width: 50,
+                        ),
+                        Text(
+                          "Pulsa",
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/pulsa.png'),
+                          width: 50,
+                        ),
+                        Text(
+                          "Pulsa",
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
